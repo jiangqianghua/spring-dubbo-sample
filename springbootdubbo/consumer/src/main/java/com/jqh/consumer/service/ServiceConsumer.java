b47ce859b49d81031d1f0423723fdd408f5a3ff9
jiangqianghua <240437339@qq.com>
2018-12-25 23:26:21 +0800

    添加zookeeper支持

--- springbootdubbo/consumer/src/main/java/com/jqh/consumer/service/ServiceConsumer.java
+++ springbootdubbo/consumer/src/main/java/com/jqh/consumer/service/ServiceConsumer.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class ServiceConsumer {
-    @Reference(url = "dubbo://127.0.0.1:20880")
+//    @Reference(url = "dubbo://127.0.0.1:20880")
+    @Reference(interfaceClass = ServiceAPI.class)
     ServiceAPI serviceAPI ;
 
     public void sendMessage(String message){
